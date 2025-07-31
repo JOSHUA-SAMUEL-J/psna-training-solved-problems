a083c6dbb5ff1dff5d3d238968e04b47608bf903
JOSHUA-SAMUEL-J <j.joshsam2k4@gmail.com>
2025-07-31 09:04:16 +0530

    Create alphabet-number-special-character.java

---
+++ alphabet-number-special-character.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.nextLine().charAt(0);
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            System.out.println("ALPHABET");
+        } else if (ch >= '0' && ch <= '9') {
+            System.out.println("NUMBER");
+        } else {
+            System.out.println("SPECIAL CHARACTER");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
