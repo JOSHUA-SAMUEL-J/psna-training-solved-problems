ec2ed542dc242b263ce39010184c2ff35c9e4a2c
JOSHUA-SAMUEL-J <j.joshsam2k4@gmail.com>
2025-07-31 09:01:56 +0530

    Create VowelorConsonant.java

---
+++ VowelorConsonant.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        char ch = sc.nextLine().charAt(0);
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
