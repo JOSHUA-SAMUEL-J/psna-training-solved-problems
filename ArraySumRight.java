142360ecf9d34ee66a5b9bd3981171717294162a
JOSHUA-SAMUEL-J <j.joshsam2k4@gmail.com>
2025-08-06 11:23:28 +0530

    Create ArraySumRight.java

---
+++ ArraySumRight.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+            sum+=arr[i];
+        }
+        
+        for (int i = 0; i < size; i++) {
+             sum -= arr[i];
+            System.out.print(sum + " ");
+        }
+      
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
