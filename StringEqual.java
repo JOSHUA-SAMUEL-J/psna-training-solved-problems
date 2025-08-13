455d65933d5f3930d043c6c898deb025aa28ee6c
JOSHUA-SAMUEL-J <j.joshsam2k4@gmail.com>
2025-08-13 09:28:15 +0530

    Create StringEqual.java

---
+++ StringEqual.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextLine().equals(s.nextLine()) ? "Equal" : "Not Equal");
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
